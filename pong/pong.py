3cce17480cf0501944001dfb9ea10053c2a4b3cb
Rodrigo Robles <rodrigoova2010@hotmail.com>
2025-03-17 23:19:07 -0400

    feat: creamos la clase pelota

--- pong/pong.py
+++ pong/pong.py
@@ -5,7 +5,7 @@ import pygame
 ANCHO_PANTALLA = 800
 ALTO_PANTALLA = 600
 #colores          rojo  verde    azul
-COLOR_DE_FONDO = (40,     60,     100)
+COLOR_DE_FONDO = (50,     50,     50)
 
 #configuraciones jugadores
 # jugador 1
@@ -22,21 +22,33 @@ MARGEN_JUGADOR2 = ANCHO_PANTALLA - MARGEN_JUGADOR - ANCHO_JUGADOR2
 COLOR_JUGADOR2 = COLOR_JUGADOR1
 
 # pelota (rectangulo)
-ALTO_PELOTA = 12
+ALTO_PELOTA = 8
 ANCHO_PELOTA = ALTO_PELOTA
 Y_PELOTA = (ALTO_PANTALLA/2) - (ALTO_PELOTA/2)
 X_PELOTA = (ANCHO_PANTALLA - ANCHO_PELOTA) /2
-COLOR_PELOTA = (100, 250, 115)
+COLOR_PELOTA = (200, 200, 200)
 
 #############################################################################
+
+class Pelota:
+    def __init__(self):
+        pass
+
+    def pintar (self, pantalla):
+        pelota = pygame.Rect(X_PELOTA, Y_PELOTA, ANCHO_PELOTA, ALTO_PELOTA)
+        pygame.draw.rect(pantalla, COLOR_PELOTA, pelota)
+    
+
 
 class Pong:
     def __init__(self):
         pygame.init()
         self.pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
+        self.pelota = Pelota()
         
 
     def jugar(self):
+
         salir = False
         eje_x = 0
         eje_y = 0
@@ -68,14 +80,34 @@ class Pong:
             jugador2 = pygame.Rect(MARGEN_JUGADOR2, MARGEN, ANCHO_JUGADOR2, ALTO_JUGADOR2)
             pygame.draw.rect(self.pantalla, COLOR_JUGADOR2, jugador2)
 
+            # pintamos la red
+            self.pinto_red()
+
             #pintamos la pelota en el centro de la pantalla
-            pelota = pygame.Rect(X_PELOTA, Y_PELOTA, ANCHO_PELOTA, ALTO_PELOTA)
-            pygame.draw.rect(self.pantalla, COLOR_PELOTA, pelota)
+            self.pelota.pintar(self.pantalla)
 
             # 3 mostrar los cambios en la pantalla
             pygame.display.flip()
 
         pygame.quit()
+
+    def pinto_red(self):
+        """pinta l red en la pantalla"""
+        # esta centrada horizontalmente
+        X_RED = ANCHO_PANTALLA / 2 - 1
+        # es un conjunto de lineas discontinuas
+        LINEA = 20
+        VACIO = 15
+        ANCHO_RED = 5
+        # ocupa todo el alto de la pantalla
+        for pos_y in range(0, ALTO_PANTALLA, LINEA+VACIO):
+            pygame.draw.line(self.pantalla, COLOR_JUGADOR1, (X_RED, pos_y), (X_RED, pos_y + LINEA), ANCHO_RED)
+            """(
+                self.pantalla,
+                COLOR_JUGADOR1,
+                (X_RED, pos_y),
+
+            )""" 
 
 juego = Pong()
 juego.jugar()
